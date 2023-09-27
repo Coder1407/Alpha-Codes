b3f7335a045d2663e215b57e3b7796ff64914ff1
Coder1407 <121633636+Coder1407@users.noreply.github.com>
2023-09-27 11:41:59 +0530

    Add files via upload

---
+++ 9. Operators/1. Binary Operators.java
@@ -0,0 +1,13 @@
+class BOperators {
+    public static void main(String args[]) {
+        int A = 10, B = 5;
+        System.out.println("add = " + (A+B));
+        System.out.println("subtract = " + (A-B));
+        System.out.println("multiply = " + (A*B));
+        System.out.println("divide = " + (A/B));
+        System.out.println("modulo(remainder) = " + (A%B));
+
+        int a = 32790;
+        System.out.println(a);
+    }
+}
